243514d240e975feb2e155400edcf139ab422f82
aaronshivers <aaron@moonmail.org>
2019-04-15 10:20:50 -0500

    updated links in navbar

--- public/navbar-script.js
+++ public/navbar-script.js
@@ -1,8 +1,8 @@
 const navLinks = document.getElementById('navLinks')
 console.log(document.cookie)
-document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/login">Login</a></li>`
-document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/signup">Signup</a></li>`
+document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/users/login">Login</a></li>`
+document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/users/signup">Signup</a></li>`
 // document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/users">Users</a></li>` : null
 document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/admin">Admin</a></li>` : null
-document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/profile">Profile</a></li>` : null
-document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>` : null
+document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/users/profile">Profile</a></li>` : null
+document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/users/logout">Logout</a></li>` : null
