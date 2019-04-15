98f66938ea9302ba584c3e8062ec4765ec27b214
aaronshivers <aaron@moonmail.org>
2019-04-15 07:40:13 -0500

    updated dotenv and expect

--- routes/users.js
+++ routes/users.js
@@ -246,7 +246,6 @@ router.delete('/delete', authenticateUser, async (req, res) => {
   try {
     const id = await verifyToken(token)
     const user = await User.findByIdAndDelete(id)
-    console.log(user.email)
     sendCancelationEmail(user.email)
     res.clearCookie('token').redirect('/blogs')
   } catch (error) {
